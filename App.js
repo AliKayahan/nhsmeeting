c3d41cd5312cdff21613a34ea271c19b53f162ac
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-02-28 11:29:52 +0000

    Text update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>chocolate</Text>
+      <Text>Hello Ahmad</Text>
     </View>
   );
 }
