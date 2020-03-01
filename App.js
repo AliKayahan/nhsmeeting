37bf0e2b113290e81f929978a65b9e5eefe909a9
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-01 22:18:19 +0000

    Adds font support to the application

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'Frutiger': require('./assets/fonts/Frutiger-Regular.otf'),
+    'Frutiger-Bold': require('./assets/fonts/Frutiger-Bold.otf'),
+    'Frutiger-Italic': require('./assets/fonts/Frutiger-Italic.otf'),
+    'Frutiger-Light-Bold': require('./assets/fonts/Frutiger-Light-Bold.otf'),
+    'Frutiger-Roman-Bold': require('./assets/fonts/Frutiger-Roman-Bold.otf')
+  });
+}
 
 export default function App() {
   return (
