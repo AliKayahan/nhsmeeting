95ca92a346b1c324239315619aeca5ddcca04862
benbb25 <61324403+benbb25@users.noreply.github.com>
2020-03-02 22:15:47 +0000

    Add files via upload
    A basic forgotten password screen which features a heading, text input box and button; no backend functionality is implemented yet.

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import {AppLoading} from 'expo';
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'Frutiger': require('./assets/fonts/Frutiger-Regular.otf'),
-    'Frutiger-Bold': require('./assets/fonts/Frutiger-Bold.otf'),
-    'Frutiger-Italic': require('./assets/fonts/Frutiger-Italic.otf'),
-    'Frutiger-Light-Bold': require('./assets/fonts/Frutiger-Light-Bold.otf'),
-    'Frutiger-Roman-Bold': require('./assets/fonts/Frutiger-Roman-Bold.otf')
-  });
-}
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);// Do not start the app without loading fonts
-
-  if(!fontsLoaded){
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontsLoaded(true)} />
-  }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.fontTest}>Hello from NHS Meeting Room App</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -30,11 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  fontTest:{
-    fontFamily: 'Frutiger'
-  }
 });
