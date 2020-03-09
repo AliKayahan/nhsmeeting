c1f5489f11ab9bfcaab8754f72bcfa3736a33750
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-09 23:14:30 +0000

    Adds login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,54 @@
 import React from 'react';
-import {View, TextInput, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {TextInput} from 'react-native-paper';
 import Background from '../components/Background';
-
+import NHSStyle from '../constants/NHSStyle';
+import Theme from '../constants/Theme';
 
 const LoginScreen = () => {
     return(
         <Background>
-            <Text>This is login screen</Text>
+            <View style={styles.logoContainer}>
+                <Image 
+                    source={require('../assets/images/NHSLogo.png')} 
+                    />
+            </View>
+            <View style={styles.bodyContainer}>
+                <Text style={NHSStyle.header1}>
+                    Welcome{"\n"}Back
+                </Text>
+                <View style={styles.inputContainer}>
+                    <TextInput 
+                        style={NHSStyle.input}
+                        theme={{
+                            colors: {
+                                primary: Theme.color.blue2,
+                                background: Theme.color.background,
+                                placeholder: Theme.color.grey5,
+                                text: Theme.color.black
+                            }
+                        }}
+                        mode='outlined'
+                        label='E-mail'
+                    />
+                </View>
+            </View>
         </Background>
     );
 };
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    logoContainer: {
+        marginLeft: 41,
+        marginTop: 67
+    },
+    bodyContainer: {
+        marginLeft: 41,
+        marginTop: 75
+    },
+    inputContainer:{
+        marginTop: 20
+    }
 });
 
 export default LoginScreen;
