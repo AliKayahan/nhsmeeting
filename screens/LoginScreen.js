359358a19df07a9afe3e6af77978ffbf605233a7
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-09 21:58:51 +0000

    Adds dummy login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, TextInput, Text, StyleSheet} from 'react-native';
+import Background from '../components/Background';
+
+
+const LoginScreen = () => {
+    return(
+        <View>
+            <Text>This is login screen</Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+
+});
+
+export default LoginScreen;
