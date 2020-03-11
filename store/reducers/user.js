8151c85de3f5748ab1f0a527b19b207c10aa5c2b
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-11 18:38:13 +0000

    user reducer fix

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -3,7 +3,7 @@ import {USER_AUTH} from '../actions/user';
 /**
  * We are setting initial user state here.
  * During development we can set data manually
- * For production revert to visitor info.
+ * For production revert to visitor.
  */
 const initialState = {
     user: {
@@ -23,7 +23,7 @@ const initialState = {
  */
 const userReducer = (state = initialState, action) => {
     switch(action.type){
-        case USER_AUTH:
+        case USER_AUTH: // Define user data action cases here
             return {...state}
         default: 
             return state
