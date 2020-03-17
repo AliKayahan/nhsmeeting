2ba3362eac2be1a31c2ffa22f5dcaa652f6cf43f
hanzen <hanzen>
2020-03-17 01:09:45 +0000

    Added more files and made some start to every pages addressed to me

---
+++ screens/Contact.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import Background from '../components/Background';
+import NHSStyle from '../constants/NHSStyle';
+import Theme from '../constants/Theme';
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <Vıew>
+                
+            </Vıew>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+        
+    }
+})
