020e4bbde5c6b9288974bc1e86d1d357f91d9728
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-24 19:45:37 +0000

    Adds profile picture to meeting listing screen

--- screens/meeting/ListMeetingScreen.js
+++ screens/meeting/ListMeetingScreen.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import CustomHeaderButton from '../../components/ui/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import Background from '../../components/Background';
+import NHSStyle from '../../constants/NHSStyle';
+import Theme from '../../constants/Theme';
 
 const ListMeetingScreen = (props) =>{
     return(
         <Background>
-            <Text>List Meetings!</Text>
+            <View style={styles.headerContainer}>
+                <View style={styles.textContainer}>
+                    <Text style={NHSStyle.mediumHeader}>Hi John,</Text>
+                    <Text style={NHSStyle.bigText}>April, 25 Monday</Text>
+                </View>
+                <View style={styles.profilePicShadow}>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.profilePic} source={{uri: 'https://randomuser.me/api/portraits/men/17.jpg'}} />
+                    </View>
+                </View>
+            </View>
         </Background>
     );
 };
 
 const styles = StyleSheet.create({
-
+    headerContainer: {
+        marginTop: 20,
+        paddingLeft: 20,
+        flex:1,
+        flexDirection: 'row'
+    },
+    textContainer:{
+        width: 280
+    },
+    profilePicShadow: {
+        shadowColor: Theme.color.black,
+        shadowOpacity: 0.25,
+        shadowOffset: {width: 2, height: 2},
+        shadowRadius: 10,
+    },  
+    imageContainer: {
+        marginLeft: 5,
+        borderWidth: 5,
+        borderColor: Theme.color.white,
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    profilePic: {
+        width:80,
+        height:80
+    }
 });
 
 export default ListMeetingScreen;
