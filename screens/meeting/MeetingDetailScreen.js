b41c8aeca55b441189538752d50a56070cf7709e
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-03-27 20:52:28 +0000

    Adds meeting detail screen

--- screens/meeting/MeetingDetailScreen.js
+++ screens/meeting/MeetingDetailScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Svg,Path} from 'react-native';
+import Background from '../../components/Background';
+import Theme from '../../constants/Theme';
+import NHSStyle from '../../constants/NHSStyle';
 
-const MeetingDetailScreen = () =>{
+const MeetingDetailScreen = props => {
     return(
-        <View>
-            <Text>Meeting Details Here..</Text>
-        </View>
-    );
-};
+        <Background>
+                <View style={styles.meetingContainer}>
+                    <View style={styles.meetingTextContainer}>
+                        <View style={styles.headerContainer}>
+                            <View style={styles.borderLine}></View>                   
+                            <Text style={[NHSStyle.bigTextBold, {color: Theme.color.black, paddingTop: 10}]}>Month Planning</Text>
+                        </View>
+                        <Text style={[NHSStyle.mediumText, {paddingLeft: 15, paddingTop: 5}]}>Business Meeting</Text>
+                    </View>
+                </View>
+        </Background>
+    )
+}
 
 const styles = StyleSheet.create({
-
-});
-
+    
+})
 export default MeetingDetailScreen;
