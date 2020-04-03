68fd4e9ec370a26e330e7cbd74662c328d0be820
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-03 15:39:27 +0100

    Adds map marker stylings

--- screens/meeting/LocationMapScreen.js
+++ screens/meeting/LocationMapScreen.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, Dimensions} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView,{ Marker } from 'react-native-maps';
 
 const LocationMapScreen = () =>{
+    const dummyMarkers = [
+        {  
+            key: 'ABCD',
+            title: 'Ali Kayahan',
+            description: 'Home',
+            latlng:{
+                latitude: 52.582972,
+                longitude: -2.119449,
+            }
+        }
+    ];
+    const [markers, setMarkers] = useState(dummyMarkers);
     return(
         <View style={styles.container}>
-            <MapView style={styles.mapStyle} />
+            <MapView style={styles.mapStyle}>
+                {markers.map(marker => (
+                    <Marker
+                        key={marker.key}
+                        coordinate={marker.latlng}
+                        title={marker.title}
+                        description={marker.description}
+                    >
+                        
+                    </Marker>
+                ))}
+            </MapView>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    mapStyle: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
-  });
+    profilePicShadow: {
+        shadowColor: Theme.color.black,
+        shadowOpacity: 0.50,
+        shadowOffset: {width: 2, height: 2},
+        shadowRadius: 10,
+    },  
+    imageContainer: {
+        borderWidth: 2,
+        borderColor: Theme.color.white,
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
+    profilePic: {
+        width:52,
+        height:52
+    },
+});
 
 export default LocationMapScreen;
