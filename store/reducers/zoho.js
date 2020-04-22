a48d65654cffa4540954eb773fcd810065e0f765
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-22 23:13:46 +0100

    Adds back-end reducer

---
+++ store/reducers/zoho.js
@@ -0,0 +1,13 @@
+/**
+ * This state holds uptodate access token 
+ * for back-end interactions.
+ */
+
+const initialState = {
+    accessToken: "",
+    lastUpdate: Date.now()
+}
+
+const backEndReducer = (state = initialState, action) => {
+    
+}
