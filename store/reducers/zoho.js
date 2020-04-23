7f281ccad174d4695dc53ef9c50a61932488dcb1
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-23 23:25:57 +0100

    Adds back-end token generator reducers

--- store/reducers/zoho.js
+++ store/reducers/zoho.js
@@ -3,11 +3,20 @@
  * for back-end interactions.
  */
 
+import { REFRESH_TOKEN } from "../actions/zoho";
+
 const initialState = {
     accessToken: "",
     lastUpdate: Date.now()
 }
 
 const backEndReducer = (state = initialState, action) => {
-    
+    switch(action.type){
+        case REFRESH_TOKEN: 
+            return {...state}
+        default:
+            return state;
+    }
 }
+
+export default backEndReducer;
