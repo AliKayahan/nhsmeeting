d7fe3f03c0482dd7a0744d94ab1fe4bd16c8a173
Benbb25 <benblackford1@gmail.com>
2020-04-23 21:10:08 +0100

    ios background shadow fix

--- components/Background.js
+++ components/Background.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 /**
  * The generic background image used during the application
- * @param {*} props 
+ * @param {*} props
  */
-const Background = props => {
-    return(
-        <ImageBackground 
-            style={{...styles.bgImageContainer, ...props.style}} // Lets us to gather further styling via props
-            source={require('../assets/images/bg.png')} 
-            imageStyle={{resizeMode:'stretch'}}>{props.children}</ImageBackground>
-    );
-}
+const Background = (props) => {
+  return (
+    <ImageBackground
+      style={{ ...styles.bgImageContainer, ...props.style }} // Lets us to gather further styling via props
+      source={require("../assets/images/bg.png")}
+      imageStyle={{ resizeMode: "stretch" }}
+    >
+      {props.children}
+    </ImageBackground>
+  );
+};
 const styles = StyleSheet.create({
-    bgImageContainer: {
-        width: '100%',// A width and height is mandatory
-        height: '100%',
-        flex: 1
-    }
+  bgImageContainer: {
+    width: "100%", // A width and height is mandatory
+    height: "100%",
+    flex: 1,
+    elevation: 0,
+  },
 });
 
 export default Background;
