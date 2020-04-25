edbfcf1427cb71b22ded8c56884269347f678fe8
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-25 19:03:28 +0100

    Fetches rooms from backend

--- screens/room/ListRoomScreen.js
+++ screens/room/ListRoomScreen.js
@@ -6,6 +6,23 @@ import RoomCard from '../../components/room/RoomCard';
 // We are planning to list and manage room state within this function
 const ListRoomScreen = props => {
     //Select rooms from redux store
+    fetch("https://www.zohoapis.com/crm/v2/coql", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Zoho-oauthtoken 1000.227a30d61df73df6ec238dab519fa97c.f0bf1dc8b6f65e66aa17c33d6b573700'
+        },
+        body: JSON.stringify({
+            "select_query" : "select Name from Facilities where City = 'Wolverhampton'"
+        })
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json.data);
+    })
+    // .catch((err) => {
+    //     console.log(err);
+    // })
     const rooms = useSelector(state => state.rooms.availableRooms);
     return (
         <View style={styles.roomCardContainer}>
