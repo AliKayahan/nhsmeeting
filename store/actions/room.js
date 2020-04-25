0e8e108d12b1d8555be18c842ab5716c8bc3edeb
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-25 21:51:41 +0100

    Replaces dyanmic icon texts with fonts

--- store/actions/room.js
+++ store/actions/room.js
@@ -7,18 +7,24 @@ export const fetchRooms = (city) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Zoho-oauthtoken 1000.7ac2565bb686ce1b22d8e1234835a347.a69831daa8312ce4d33683b134c8b03e'
+                'Authorization': 'Zoho-oauthtoken 1000.9ffcf1132519863442c12739a6b81420.98d153e11b65b58f070ccc1d8ade30fc'
             },
             body: JSON.stringify({
                 "select_query" : "select Name, Floor, Features, Facility, Description,Images, Facility.Name, Facility.Has_Parking from Rooms where Name != 'void'"
             })
         });
-
+        ['ios-snow', 'ios-videocam', 'ios-mic', 'ios-wifi', 'ios-car']
+        const iconReplacements = {"Wifi": "ios-wifi", "Air Conditioner": "ios-snow", "Projector": "ios-videocam","Cafe":"ios-cafe", "Tv": "ios-tv", "Microphone": "ios-mic","ios-car":"ios-car"}
         const resData = await response.json();
         const matchingRooms = [];
         console.log(resData)
         resData.data.map((val) => {
-            console.log(val);
+            const images = val["Images"].split(",");
+            let featuresText = val["Facility.Has_Parking"] ? [...val["Features"],'ios-car'] : val["Features"];
+            let featuresIcon = []
+            featuresText.map((val) => {
+                featuresIcon.push(iconReplacements[val])
+            });
         });
         //console.log(resData.data);
         dispatch({
