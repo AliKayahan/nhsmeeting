49b186ecfb508368143b4005619e9fe25e934056
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-04-25 20:51:49 +0100

    Adds dynamic room listing

--- store/reducers/room.js
+++ store/reducers/room.js
@@ -1,10 +1,17 @@
+import { FETCH_ROOMS } from '../actions/room';
 import ROOMS from '../../data/dummy-data';
 //Initial state can be all rooms and filtered rooms
 const initialState = {
-    availableRooms: ROOMS,
-    filteredRooms: ROOMS.filter(room => room.type === 'Class')
+    availableRooms: ROOMS
 }
+
 //We should export reducer to be use from app.js
 export default (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_ROOMS:
+            return {
+                availableRooms: action.availableRooms,
+            }
+    }
     return state;
 };
