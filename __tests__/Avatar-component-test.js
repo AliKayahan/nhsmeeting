51f496f34b0746fb28f68897f5bc06053ac124ff
Ali Kayahan <ali.kayahan@angular.com.tr>
2020-05-01 14:01:25 +0100

    Adds avatar component unit test

---
+++ __tests__/Avatar-component-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import Avatar from '../components/profile/Avatar';
+
+/**
+ * Unit test for Avatar component
+ */
+test('Avatar component renders correctly', () => {
+  const tree = renderer.create(<Avatar />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- __tests__/Login-test.js
+++ __tests__/Login-test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import LoginScreen from '../screens/user/LoginScreen';
 
 import renderer from 'react-test-renderer';
-
+/**
+ * Integration test for Login Screen
+ */
 test('Login UI renders correctly', () => {
   const tree = renderer.create(<LoginScreen />).toJSON();
   expect(tree).toMatchSnapshot();
